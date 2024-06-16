b7bb2e87fce6ecf26d21d5c1863485af4148e3ac
Michael Putong <michael.wenceslaus@gmail.com>
2024-06-16 23:24:39 +0700

    modified:   src/App.tsx         modified:   src/components/avatar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import Avatar from "./components/avatar.tsx";
 
 function App() {
     return (
-        <div className={"flex xl:flex-row flex-col items-center p-10 w-full justify-center"}>
+        <div className={"flex xl:flex-row l:flex-row flex-col items-center p-10 w-full justify-center"}>
             <div className={"p-2"}>
                 <Avatar/>
             </div>
             <div className={"m-2"}/>
-            <div className={"text-white flex flex-col font-extralight space-y-2"}>
+            <div className={"text-white flex flex-col space-y-2"}>
                 <div className={"flex flex-col m-2"}>
                     <Title/>
                 </div>
--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -4,7 +4,7 @@ const Avatar = () => {
     return (
         <>
             <img src={avatar} alt={"michael putong"}
-                 className={"xl:w-96 w-full rounded-2xl xl:border border-gray-500"}/>
+                 className={"xl:w-96 l:w-96 md:w-96 w-full rounded-2xl xl:border l:border border border-gray-500"}/>
         </>
     )
 }
